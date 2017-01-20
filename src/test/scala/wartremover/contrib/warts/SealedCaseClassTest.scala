49406444ae108bc6bc5d29f0744119c400b92f06
amanjpro <amanj.sherwany@adgear.com>
2017-01-20 13:26:05 -0500

    Add a wart for sealed case classes

---
+++ src/test/scala/wartremover/contrib/warts/SealedCaseClassTest.scala
@@ -0,0 +1,41 @@
+package org.wartremover
+package contrib.test
+
+import org.scalatest.FunSuite
+
+import org.wartremover.contrib.warts.SealedCaseClass
+import org.wartremover.test.WartTestTraverser
+
+class FinalCaseClassTest extends FunSuite {
+  test("can't declare sealed case classes") {
+    val result = WartTestTraverser(SealedCaseClass) {
+      sealed case class Foo(i: Int)
+    }
+    assertResult(List("case classes must not be sealed"), "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+  test("can declare non-sealed case classes") {
+    val result = WartTestTraverser(SealedCaseClass) {
+      case class Foo(i: Int)
+    }
+    assertResult(List.empty, "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+  test("can declare sealed regular classes") {
+    val result = WartTestTraverser(SealedCaseClass) {
+      sealed class Foo(i: Int)
+      sealed trait Bar
+      sealed abstract class Baz
+    }
+    assertResult(List.empty, "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+  test("SealedCaseClass wart obeys SuppressWarnings") {
+    val result = WartTestTraverser(SealedCaseClass) {
+      @SuppressWarnings(Array("org.wartremover.contrib.warts.SealedCaseClass"))
+      sealed case class Foo(i: Int)
+    }
+    assertResult(List.empty, "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+}
