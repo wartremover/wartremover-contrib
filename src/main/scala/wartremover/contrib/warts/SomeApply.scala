5a04a3b35c570868de75796e97997326781288bc
Omer van Kloeten <omer.van.kloeten@gmail.com>
2017-02-01 22:21:14 +0200

    Added the SomeApply wart

---
+++ src/main/scala/wartremover/contrib/warts/SomeApply.scala
@@ -0,0 +1,27 @@
+package wartremover.contrib.warts
+
+import org.wartremover.{WartTraverser, WartUniverse}
+
+object SomeApply extends WartTraverser {
+  def apply(u: WartUniverse): u.Traverser = {
+    import u.universe._
+
+    val scala: TermName = "scala"
+    val some: TermName = "Some"
+    val app: TermName = "apply"
+
+    new u.Traverser {
+      override def traverse(tree: Tree): Unit = {
+        tree match {
+          // Ignore trees marked by SuppressWarnings
+          case t if hasWartAnnotation(u)(t) =>
+          case Apply(TypeApply(Select(Select(Ident(pkg), obj), method), _), _)
+            if pkg == scala && obj == some && method == app =>
+            u.error(tree.pos, "Some.apply is disabled - use Option.apply instead")
+          case v =>
+            super.traverse(tree)
+        }
+      }
+    }
+  }
+}
