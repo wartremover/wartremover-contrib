3c01ff5c6126f9f9863fda3f5da14b111d7a6510
Paul Phillips <paulp@improving.org>
2017-03-08 12:50:37 -0800

    Recursion wart.

---
+++ core/src/test/scala/wartremover/contrib/warts/RecursionTest.scala
@@ -0,0 +1,41 @@
+package org.wartremover
+package contrib.test
+
+import org.scalatest.FunSuite
+import scala.annotation.tailrec
+import org.wartremover.test.WartTestTraverser
+import org.wartremover.contrib.warts.Recursion
+
+class RecursionTest extends FunSuite with ResultAssertions {
+
+  test("can't use recursion") {
+    val result = WartTestTraverser(Recursion) {
+      def foo(x: Int): Int = foo(x)
+    }
+    assertError(result)("Unmarked recursion")
+  }
+
+  test("can't use nested recursion") {
+    val result = WartTestTraverser(Recursion) {
+      def foo(x: Int): Int = {
+        def bar(y: Int): Int = foo(y)
+        bar(1)
+      }
+    }
+    assertError(result)("Unmarked recursion")
+  }
+
+  test("can use non-recursion") {
+    val result = WartTestTraverser(Recursion) {
+      def foo(x: Int): Int = x
+    }
+    assertEmpty(result)
+  }
+
+  test("can use tail recursion") {
+    val result = WartTestTraverser(Recursion) {
+      @tailrec def foo(x: Int): Int = if (x < 0) 1 else foo(x - 1)
+    }
+    assertEmpty(result)
+  }
+}
