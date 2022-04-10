8cc55825dd224368a303fd75829d1767d5c7c483
xuwei-k <6b656e6a69@gmail.com>
2022-04-10 09:29:33 +0900

    DiscardedFuture Scala 3

---
+++ core/src/main/scala-3/org/wartremover/contrib/warts/DiscardedFuture.scala
@@ -0,0 +1,35 @@
+package org.wartremover
+package contrib.warts
+
+import scala.concurrent.Future
+
+object DiscardedFuture extends WartTraverser {
+
+  val message: String =
+    """andThen discards the return value of callback.
+      |To chain the result of Future to other Future, use flatMap.
+      |""".stripMargin
+
+  def apply(u: WartUniverse): u.Traverser = {
+    new u.Traverser(this) {
+      import q.reflect.*
+      override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
+        tree match {
+          case _ if hasWartAnnotation(tree) =>
+          case _ if tree.isExpr =>
+            tree.asExpr match {
+              case '{
+                    type t1
+                    ($x1: Future[`t1`]).andThen { ($x2: PartialFunction[scala.util.Try[`t1`], Future[t2]]) }($ec)
+                  } =>
+                error(tree.pos, message)
+              case _ =>
+                super.traverseTree(tree)(owner)
+            }
+          case _ =>
+            super.traverseTree(tree)(owner)
+        }
+      }
+    }
+  }
+}
