352d81aca2d6872d90745387a9c1645253daf30e
xuwei-k <6b656e6a69@gmail.com>
2026-01-14 16:13:54 +0900

    optimize Scala 3 RefinedClasstag

--- core/src/main/scala-3/org/wartremover/contrib/warts/RefinedClasstag.scala
+++ core/src/main/scala-3/org/wartremover/contrib/warts/RefinedClasstag.scala
@@ -18,26 +18,23 @@ object RefinedClasstag extends WartTraverser {
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
         tree match {
           case _ if hasWartAnnotation(tree) =>
-          case _ if tree.isExpr =>
-            tree.asExpr match {
-              case '{ ClassTag[t1]($clazz) } =>
-                TypeRepr.of[t1] match {
-                  case t2: AndType =>
-                    error(tree.pos, ctMessage(t2.left.show + " with " + t2.right.show))
-                  case t2: OrType =>
-                    error(tree.pos, ctMessage(t2.show))
-                  case _ =>
-                    super.traverseTree(tree)(owner)
-                }
-              case '{ ManifestFactory.classType[t1]($clazz) } =>
-                TypeRepr.of[t1] match {
-                  case t2: AndType =>
-                    error(tree.pos, mfMessage(t2.left.show + " with " + t2.right.show))
-                  case t2: OrType =>
-                    error(tree.pos, mfMessage(t2.show))
-                  case _ =>
-                    super.traverseTree(tree)(owner)
-                }
+          case Apply(TypeApply(Select(classTagObj, "apply"), t1 :: Nil), _ :: Nil)
+              if classTagObj.tpe =:= TypeRepr.of[ClassTag.type] =>
+            t1.tpe match {
+              case t2: AndType =>
+                error(tree.pos, ctMessage(t2.left.show + " with " + t2.right.show))
+              case t2: OrType =>
+                error(tree.pos, ctMessage(t2.show))
+              case _ =>
+                super.traverseTree(tree)(owner)
+            }
+          case Apply(TypeApply(Select(manifestFactoryObj, "classType"), t1 :: Nil), _ :: Nil)
+              if manifestFactoryObj.tpe =:= TypeRepr.of[ManifestFactory.type] =>
+            t1.tpe match {
+              case t2: AndType =>
+                error(tree.pos, mfMessage(t2.left.show + " with " + t2.right.show))
+              case t2: OrType =>
+                error(tree.pos, mfMessage(t2.show))
               case _ =>
                 super.traverseTree(tree)(owner)
             }
